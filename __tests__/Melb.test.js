3e6c4120268141b839761234dd5f3c43cf57ae0a
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-26 16:54:32 -0800

    first draft melb model & test

--- __tests__/Melb.test.js
+++ __tests__/Melb.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Melb = require('../lib/models/Melb');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -12,9 +13,16 @@ describe('backend routes', () => {
     pool.end();
   });
 
-  it(' ', async () => {
-    const res = await request(app).get('/');
+  it('creates a Mel B. entry ', async () => {
+    const mel = await Melb.insert({ name:'Melanie Janine Brown', stagename:'Scary Spice', birthdate:'5/29/1975' });
 
-    expect(res.status).toEqual(404);
+    const res = await request(app).get('api/v1/spiceupyourlife');
+
+    expect(res).toEqual({
+      id: expect.any(String),
+      name: 'Melanie Janine Brown',
+      stagename: 'Scary Spice',
+      birthdate: '5/29/1975'
+    });
   });
 });
