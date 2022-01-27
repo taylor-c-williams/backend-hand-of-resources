ee6c45fba7b788f793cf963544d0d7b0890c355d
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-27 14:53:08 -0800

    victoria .get, getAll, getById, tests passing

--- __tests__/Victoria.test.js
+++ __tests__/Victoria.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const app = require('../lib/app');
 const request = require('supertest');
-const Victoria = require('../lib/models/Emma');
+const Victoria = require('../lib/models/Victoria');
 
 describe('Victoria Routes', () => {
   beforeEach(() => {
@@ -22,6 +22,34 @@ describe('Victoria Routes', () => {
       stagename: 'Posh Spice',
       birthdate: '4/17/1974'
     });
+  });
+
+  it('creates a Victoria entry', async () => {
+    const victoria = await Victoria.insert({ name:'Victoria Caroline Adams', stagename:'Posh Spice', birthdate:'4/17/1974' });
+
+    expect(victoria).toEqual({
+      id: expect.any(String),
+      name: 'Victoria Caroline Adams',
+      stagename: 'Posh Spice',
+      birthdate: '4/17/1974'
+    });
+  });
+
+  it('gets all Victoria', async () => {
+    const realVictoria = await Victoria.insert({ name:'Victoria Caroline Adams', stagename: 'Posh Spice', birthdate:'4/17/1974' });
+
+    const fakeVictoria = await Victoria.insert({ name: 'Fake Victoria', stagename: 'Pubby Spice', birthdate: '1/22/5376' });
+
+    const res = await request(app).get('/api/v1/spiceupyourlife/victoria');
+    expect(res.body).toEqual([realVictoria, fakeVictoria]);
+  });
+
+  it ('fetches a single Victoria by her ID', async () => {
+    const victoria = await Victoria.insert({ name: 'Victoria Caroline Adams', stagename: 'Posh Spice', birthdate:'4/17/1974' });
+    
+    const res = await request(app).get(`/api/v1/spiceupyourlife/victoria/${victoria.id}`);
+
+    expect(res.body).toEqual(victoria);
   });
 
 
